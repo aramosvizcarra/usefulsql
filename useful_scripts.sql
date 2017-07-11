6a12ec1e05969af1cf66370e5d5c3032b8051232
aramosvizcarra <ramo0025@algonquinlive.com>
2017-07-11 16:17:30 -0400

    remove unwanted

--- useful_scripts.sql
+++ useful_scripts.sql
@@ -1,7 +1,8 @@
+--finds alltables that contain an specific column
 SELECT c.name AS ColName, t.name AS TableName
 FROM sys.columns c
     JOIN sys.tables t ON c.object_id = t.object_id
-WHERE c.name LIKE '%midas_org_id%'
+WHERE c.name LIKE '%<some important_field>%'
 
---Find the afm field columns enumerations
+--Find the afm field columns 
 select * from afm_flds where table_name='afm_flds'
